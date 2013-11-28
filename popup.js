edd784936af96165c46d83d2d9bc59d41fd69c9c
Wil Yegelwel <wyegelwel@gmail.com>
2013-11-28 13:00:46 -0500

    Loading cards, using local storage to remember token

--- popup.js
+++ popup.js
@@ -3,19 +3,32 @@ var token = null;
 function onAuthorize(){
   $("#loggedout").hide();
   $("#output").show();
-  Trello.get("members/me/boards", function(boards){
+  Trello.get("members/me/boards", function(boards_){
+    var boards = boards_;
     $.each(boards, function (idx, board){
-      board.lists = [];
+      Trello.get("boards/"+board.id+"/lists", function(lists){
+        board.lists = lists;
+        if (allBoardsLoaded(boards)){
+          setUpBoards(boards);
+        }
+      });
     });
-    setUpBoards(boards);
   });
+}
+
+function allBoardsLoaded(boards){
+  var loaded = true;
+  $.each(boards, function(idx, board){
+    loaded = loaded && board.lists;
+  });
+  return loaded;
 }
 
 function setUpBoards(boards){
   boardContainer = $("<div>").appendTo($("#output"));
   $.each(boards, function(idx, board){
     boardDiv = $("<div>").appendTo(boardContainer);
-    $("<h3>").text(board.name).appendTo(boardDiv);
+    $("<h5>").text(board.name).appendTo(boardDiv);
     $.each(board.lists, function(idx, list){
       listDiv = $("<div>").text(list.name).appendTo(boardDiv);
     });
@@ -24,12 +37,17 @@ function setUpBoards(boards){
 
 function init(){
   $("#output").hide();
-
+  token = localStorage.getItem("Trello_token");
+  if (token){
+    Trello.setToken(token);
+    onAuthorize();
+  }
 
   $("#tokenEntered").click(function(e){
       token = $.trim($("#tokenField").val());
       console.log(token);
       if (token && token.length > 0){ //change check so that it equals length expected
+        localStorage.setItem("Trello_token", token);
         Trello.setToken(token);
         onAuthorize();
       } 
