a0f3878f94b80a61cc7661cadc4c0071ea40f3e0
Wil Yegelwel <wyegelwel@gmail.com>
2013-11-28 20:31:24 -0500

    Able to add cards with URL as comment
    Should consider being able to add more comments

--- popup.js
+++ popup.js
@@ -3,12 +3,19 @@ var token = null;
 function onAuthorize(){
   $("#loggedout").hide();
   $("#trelloDiv").show();
+
+  // var boards = localStorage.getItem("Trello_boards");
+  // if (boards && boards != "null"){
+  //   setUpBoards(boards);
+  // }
+
   Trello.get("members/me/boards", function(boards_){
     var boards = boards_;
     $.each(boards, function (idx, board){
       Trello.get("boards/"+board.id+"/lists", function(lists){
         board.lists = lists;
         if (allBoardsLoaded(boards)){
+          localStorage.setItem("Trello_boards", boards)
           setUpBoards(boards);
         }
       });
@@ -24,16 +31,81 @@ function allBoardsLoaded(boards){
   return loaded;
 }
 
-function addCard(listId, name){
+function addCard(listId, card){
   Trello.post("lists/"+listId + "/cards",
-              {name: name, due:null},
-              function(){
-                console.log("WOO");
+              {name: card.name, 
+               due: null,
+               desc: card.desc
+             },
+              function(c){
+                if (card.comment){
+                  Trello.post("cards/"+c.id + "/actions/comments",
+                  {text: card.comment},
+                  function(c){
+                    console.log(c);
+                  
+                  //TODO: Add success notification
+                  
+                  });
+                }   
               });
 }
 
+
+
+function showBoardScreen(){
+  addContainer.hide();
+  boardContainer.show();
+}
+
+function setupAdd(listId){
+  addContainer = $("#addContainer").show();
+  boardContainer = $("#boardContainer").hide();
+  addContainer.empty();
+  addField = $("<input>")
+            .attr("placeholder", "Card name")
+            .appendTo($("<div>").text("Card name: ").appendTo(addContainer));
+  descField = $("<input>")
+            .attr("placeholder", "Card description")
+            .appendTo($("<div>").text("Card description: ").appendTo(addContainer));
+  siteComment = $("<input>")
+                .attr("id", "siteComment")
+                .attr("type", "checkbox")
+                .text("Include current URL as comment")
+                .appendTo($("<div>")
+                            .text("Include current URL as comment: ")
+                            .appendTo(addContainer));
+  addBtn = $("<button>")
+            .attr("id", "addConfirm")
+            .addClass("addBtn")
+            .click(function(e){
+                    chrome.tabs.getSelected(null, function(tab) {
+                        var comment = null;
+                        if ($("#siteComment").attr("checked")){
+                          comment = tab.url;
+                        }
+                        var card = {name: $.trim(addField.val()),
+                                    desc: $.trim(descField.val()),
+                                    comment: comment
+                                    };
+                        addCard(listId, card);
+                        showBoardScreen();
+                        console.log(card);
+                      });
+                  })
+            .text("Add")
+            .appendTo(addContainer);
+  cancelBtn = $("<button>")
+            .attr("id", "addCancel")
+            .addClass("addBtn")
+            .click(showBoardScreen)
+            .text("Cancel")
+            .appendTo(addContainer);
+}
+
 function setUpBoards(boards){
-  boardContainer = $("<div>").appendTo($("#trelloDiv"));
+  boardContainer = $("#boardContainer");
+  boardContainer.empty();
   $("<div>").text("Add card to:").appendTo(boardContainer);
   $.each(boards, function(idx, board){
     console.log(board);
@@ -49,23 +121,27 @@ function setUpBoards(boards){
                 .text(list.name)
                 .addClass("list")
                 .attr("name", list.id)
-                .click(function(e){addCard($(e.target).attr("name"), "test");})
+                .click(function(e){setupAdd($(e.target).attr("name"));})
                 .appendTo(boardDiv);
     });
   });
 }
 
 function init(){
+  //initial hidden elements
   $("#trelloDiv").hide();
+  $("#addContainer").hide();
+
+  //load token
   token = localStorage.getItem("Trello_token");
   if (token && token != "null"){
     Trello.setToken(token);
     onAuthorize();
   }
 
+  //Set up click listeners
   $("#tokenEntered").click(function(e){
       token = $.trim($("#tokenField").val());
-      console.log(token);
       if (token && token.length > 0){ //change check so that it equals length expected
         localStorage.setItem("Trello_token", token);
         Trello.setToken(token);
